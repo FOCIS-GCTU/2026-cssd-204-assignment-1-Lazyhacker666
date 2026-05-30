6402dc0e04c42440df5020d33204514a854c1380
Lazyhacker666 <2425403000@live.gctu.edu.gh>
2026-05-30 18:12:51 -0400

    Add files via upload
    2425403000 Kofi Bekoe Oduro

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out initials in large block letters
+# Assignment Number: 1
+#
+# Name: KOFI BEKOE ODURO
+# STUDENT ID: 2425403000
+# Email: KBEKOE233@GMAIL.COM
+# Grader: MR. AUGUSTUS BUCKMAN
+#
+#On my honor, KOFI BEKOE ODURO, this programming assignment is my own work
+#and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
 
+    print()
+    print("...KBO")
+    print()
+    print("...KK......KKKK........BBBBBBBB..........OOOOOOOOOO......")
+    print("...KK.....KKKK.........BBBBBBBBB........OOOOOOOOOOOO.....")
+    print("...KK....KKKK..........BB......BB.......OO........OO.....")
+    print("...KK...KKK............BB......BB.......OO........OO.....")
+    print("...KKKKKKK.............BBBBBBBBB........OO........OO.....")
+    print("...KKKKKKK.............BBBBBBBBB........OO........OO.....")
+    print("...KK...KKK............BB......BB.......OO........OO.....")
+    print("...KK....KKK...........BB......BB.......OO........OO.....")
+    print("...KK.....KKKK....**...BBBBBBBBB...**...OOOOOOOOOOOO...**")
+    print("...KK......KKKK...**...BBBBBBBB....**....OOOOOOOOOO....**")
+    print()
 
 
 main()
